1c571895ed0e49192b0cb079efee6ef231392d22
Ethan Fan <coolioxlr@gmail.com>
2016-04-16 17:54:07 -0700

    Initial Commit

---
+++ cardgame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cardgame
+//
+//  Created by Ethan Fan on 4/16/16.
+//  Copyright © 2016 tvoshack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
